2f93ac77a0db7e99753aaa2f56d619e49d8b11b9
anjuma <anjuma.sk11@gmail.com>
2025-05-31 21:20:29 +0530

    shellroboshop

---
+++ frontend.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+source ./commom.sh
+
+
+check_root
+
+dnf module disable nginx -y &>>$LOG_FILE
+VALIDATE $? "Disabling default nginx"
+
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+VALIDATE $? "Enabling nginx:20"
+
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing nginx:1.24"
+
+systemctl enable nginx
+VALIDATE $? "Enabling  nginx"
+
+systemctl start nginx
+VALIDATE $? "Started nginx"
+
+rm -rf /usr/share/nginx/html/*
+VALIDATE $? "Removing default html"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading frontend code"
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "unzipping frontend"
+
+rm -rf /etc/nginx/nginx.conf
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Updating nginx conf file"
+
+systemctl restart nginx
+VALIDATE $? "restarting nginx"
+
+
+print_time
