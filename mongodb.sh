e49e933517fe8eb439afa604a1824783a1bf6a81
anjuma <anjuma.sk11@gmail.com>
2025-05-31 21:56:37 +0530

    shellroboshop

--- mongodb.sh
+++ mongodb.sh
@@ -3,9 +3,9 @@
 source ./common.sh
 app_name=mongodb
 
-check_root()
+check_root
 
-cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongodb.repo
+cp mongo.repo /etc/yum.repos.d/mongodb.repo
 VALIDATE $? "Copying mongo repo file"
 
 dnf install mongodb-org -y
@@ -17,7 +17,7 @@ VALIDATE $? "Enabling mongod"
 systemctl start mongod
 VALIDATE $? "Starting mongod"
 
-sed -e 's/127.0.0.-1/0.0.0.0/g' /etc/mongod.conf
+sed -i 's/127.0.0.-1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Updating mongod file to allow remote connections"
 
 systemctl restart mongod
