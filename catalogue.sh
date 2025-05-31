492467d073da96935301dd2f34af4072129c61aa
anjuma <anjuma.sk11@gmail.com>
2025-05-31 22:34:14 +0530

    shellroboshop

--- catalogue.sh
+++ catalogue.sh
@@ -19,7 +19,7 @@ VALIDATE $? "Installing mongodb client"
 STATUS=$(mongosh --host mongodb.anjuma.store --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host mongodb.anjuma.store </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
